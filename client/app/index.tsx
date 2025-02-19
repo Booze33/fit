6227ed0df83225c39c52bb7c8345995557744529
Booze33 <tebetisloh@gmail.com>
2025-02-19 15:43:28 +0100

    routes

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,13 +1,47 @@
-import { StyleSheet, Image, Platform, Text, View } from 'react-native';
+import { StyleSheet, Image, Platform, Text, Button, View } from 'react-native';
+import { GetLoggedInUser } from '@/lib/auth/index';
+import { useEffect, useState } from 'react';
+import { router, Link } from 'expo-router';
 import './global.css';
 
 export default function index() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        setLoading(true);
+        const user = await GetLoggedInUser();
+        if (user) {
+          router.replace('/workout');
+        }
+      } catch (error) {
+        console.error('Error checking authentication status:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
 
   return (
-    <>
+    <View className="w-[100vw] h-[100vh] flex justify-center items-center bg-gray-400 flex-col">
       <View>
-        <Text className="bg-red-600 text-[4rem]">Check</Text>
+        <Link
+          href="/auth/sign_up"
+          className="text-lg text-center"
+        >
+          Don't have an account?{" "}
+          <Text className="text-primary-500">Sign Up</Text>
+        </Link>
+        <Link
+          href="/auth/sign_in"
+          className="text-lg text-center"
+        >
+          <Text className="text-primary-500">Sign In</Text>
+        </Link>
       </View>
-    </>
+    </View>
   );
 }
